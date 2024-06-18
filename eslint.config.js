8582bc7b444af10973d6bb5007e4af261a6114cd
AlvaroBk <49281436+AlvaroBk@users.noreply.github.com>
2024-06-18 11:48:18 +0200

    secOps 1

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,21 @@
 // eslint.config.js
-import { Linter } from "eslint";
+import { ESLint } from "eslint";
 
-/** @type {Linter.Config} */
-const config = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        browser: true,
+        node: true,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+    },
+    rules: {
+      // Add your custom rules here
+    },
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["@typescript-eslint"],
-  rules: {
-    // Add your custom rules here
-  },
-};
-
-export default config;
+];
