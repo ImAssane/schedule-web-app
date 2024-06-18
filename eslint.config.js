3cc9cacf7630caf954496753658f59032305e7bb
AlvaroBk <49281436+AlvaroBk@users.noreply.github.com>
2024-06-18 11:30:09 +0200

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import { Linter } from "eslint";
+
+/** @type {Linter.Config} */
+const config = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    // Add your custom rules here
+  },
+};
+
+export default config;
